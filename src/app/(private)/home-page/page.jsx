6e6d3469f3907d7ea9d667aa6330480cfaa783f0
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2024-02-03 17:43:41 +0700

    upload

--- src/app/(private)/home-page/page.jsx
+++ src/app/(private)/home-page/page.jsx
@@ -1,18 +1,28 @@
-'use client'
-import dynamic from 'next/dynamic'
-const VideoRoom = dynamic(() => import('@/components/call/videoRoom'), { ssr: false });
-import React, { useState } from 'react'
+// 'use client'
+// import dynamic from 'next/dynamic'
+// const VideoRoom = dynamic(() => import('@/components/call/videoRoom'), { ssr: false });
+// import React, { useState } from 'react'
+
+// const HomePage = () => {
+
+//     const [joined, setJoined] = useState(false)
+
+//     return (
+//         <div className='w-full flex flex-col items-center justify-center h-screen' >
+//             <h1>QiChat</h1>
+//             {!joined && (<button onClick={() => setJoined(true)}>Join</button>)}
+//             {joined && (<VideoRoom />)}
+//         </div>
+//     )
+// }
+
+// export default HomePage
+
+import React from 'react'
 
 const HomePage = () => {
-
-    const [joined, setJoined] = useState(false)
-
     return (
-        <div className='w-full flex flex-col items-center justify-center h-screen' >
-            <h1>QiChat</h1>
-            {!joined && (<button onClick={() => setJoined(true)}>Join</button>)}
-            {joined && (<VideoRoom />)}
-        </div>
+        <div>HomePage</div>
     )
 }
 
